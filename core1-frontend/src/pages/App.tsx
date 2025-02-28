a5713c27b25c73c4752844f0a900afae86d0223c
Chysev <Johnlayda92@gmail.com>
2025-02-28 22:43:28 +0800

    fix: Layout

--- core1-frontend/src/pages/App.tsx
+++ core1-frontend/src/pages/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { Link, useNavigate } from '@tanstack/react-router';
 
-import Layout from '../components/Layout/Layout';
 import Button from '../components/Buttons/Button';
 import { isNotAuthenticated } from '../lib/useToken';
 
@@ -13,16 +12,13 @@ const App = () => {
   }, []);
 
   return (
-    <Layout>
-      <div className="items-center flex-col flex bg-[#131313] p-5 rounded-lg">
-        <img src="AuthLogo.png" className=" h-[200px]" alt="" />
-        <div className="grid gap-2 w-full">
-          <Button>
-            <Link to="/auth/login">Login</Link>
-          </Button>
-        </div>
+    <div className="items-center flex-col flex bg-[#131313] p-5 rounded-lg">
+      <div className="grid gap-2 w-full">
+        <Button>
+          <Link to="/auth/login">Login</Link>
+        </Button>
       </div>
-    </Layout>
+    </div>
   );
 };
 
